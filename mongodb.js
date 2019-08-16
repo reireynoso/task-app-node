abe01b69618142ce2c8ecbb56a5e5e4e2eb577d7
github username <github email address>
2019-08-15 21:06:56 -0400

    changed database name

--- mongodb.js
+++ mongodb.js
@@ -8,7 +8,7 @@ const {MongoClient, ObjectID} = require('mongodb')
 
 // const connectionURL = 'mongodb://127.0.0.1:27017'
 
-const databaseName = 'task-manager'
+const databaseName = 'task-manager-api'
 
 // const id = new ObjectID()
 // console.log(id)
